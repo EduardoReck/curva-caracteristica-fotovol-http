f003de857125302d7477984f7c38540f21701008
EduardoReck <eduardorecktenwald11@gmail.com>
2024-12-06 19:25:52 -0300

    Delete envia.py

--- envia.py
+++
@@ -1,9 +0,0 @@
-from SimCell import celula
-from SimCell2 import celula_1
-def send(entrada1, entrada2, entrada3, entrada4): 
-    
-  global V, Pplot, Iplot, Pplot2, Iplot2
-  print("chegou em send")
-  celula(entrada1, entrada2, entrada3, entrada4)
-  
-  celula_1(entrada1, entrada2, entrada3, entrada4)
